f7889dbc3ead4b38fb031c3b5849bd8538f850aa
little-junior <rampagedolol@outlook.com>
2023-12-01 17:52:42 -0300

    edit JogoDaForca project

---
+++ JogoDaForcaSolution/JogoDaForca/Dados/DadosParaUso.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JogoDaForca.Dados
+{
+    internal class DadosParaUso
+    {
+        public static Dictionary<string, string> dictPalavraTipo = new()
+        {
+            {"Brasil", "País" },
+            {"Carne", "Comida" },
+            {"Cachorro", "Animal"},
+            {"Maçã", "Fruta"},
+            {"Computador", "Eletrônico"},
+            {"Montanha", "Geografia"},
+            {"Avião", "Meio de Transporte"},
+            {"Livro", "Objeto"},
+            {"Piano", "Instrumento Musical"},
+            {"Sapato", "Vestuário"},
+            {"Oceano", "Geografia"},
+            {"Elefante", "Animal"},
+            {"Bicicleta", "Meio de Transporte"},
+            {"Árvore", "Vegetal"},
+            {"Telefone", "Eletrônico"},
+            {"Pintura", "Arte"},
+            {"Pizza", "Comida"},
+            {"Sol", "Corpo Celeste"},
+            {"Óculos", "Acessório"},
+            {"Rio", "Geografia"},
+            {"Foguete", "Meio de Transporte"},
+            {"Relógio", "Acessório"}
+        };
+    }
+}
--- JogoDaForcaSolution/JogoDaForca/Jogo.cs
+++ JogoDaForcaSolution/JogoDaForca/Jogo.cs
@@ -1,39 +1,16 @@
 ﻿namespace JogoDaForca
 {
+    using Dados;
+    using Biblioteca;
     internal class Jogo
     {
         static void Main(string[] args)
         {
-            Dictionary<string, string> dict = new()
-            {
-                {"Brasil", "País" },
-                {"Carne", "Comida" },
-                {"Cachorro", "Animal"},
-                {"Maçã", "Fruta"},
-                {"Computador", "Eletrônico"},
-                {"Montanha", "Geografia"},
-                {"Avião", "Meio de Transporte"},
-                {"Livro", "Objeto"},
-                {"Piano", "Instrumento Musical"},
-                {"Sapato", "Vestuário"},
-                {"Oceano", "Geografia"},
-                {"Elefante", "Animal"},
-                {"Bicicleta", "Meio de Transporte"},
-                {"Árvore", "Vegetal"},
-                {"Telefone", "Eletrônico"},
-                {"Pintura", "Arte"},
-                {"Pizza", "Comida"},
-                {"Sol", "Corpo Celeste"},
-                {"Óculos", "Acessório"},
-                {"Rio", "Geografia"},
-                {"Foguete", "Meio de Transporte"},
-                {"Relógio", "Acessório"}
-            };
             while (true)
             {
                 Console.Clear();
-                var numAleatorio = new Random().Next(0, dict.Count);
-                var aleatorio = dict.ElementAt(numAleatorio);
+                var numAleatorio = new Random().Next(0, DadosParaUso.dictPalavraTipo.Count);
+                var aleatorio = DadosParaUso.dictPalavraTipo.ElementAt(numAleatorio);
 
                 var palavra = aleatorio.Key;
                 var temaPalavra = aleatorio.Value;
@@ -41,8 +18,8 @@
                 string palavraVazia = new('_', palavra.Length);
                 char[] palavraVaziaVetor = palavraVazia.ToCharArray();
 
-                bool acertou = false, tentativasAcabaram = false;
-                bool verificarEntrada;
+                bool acabou = false;
+                bool entrada, resultadoEntrada;
                 int tentativas = 5;
                 List<char> letrasDigitadas = [];
 
@@ -50,62 +27,27 @@
                 {
                     Console.WriteLine($"Tema da palavra: {temaPalavra}");
                     Console.WriteLine($"Tentativas restantes: {tentativas}\n");
-                    Array.ForEach(palavraVaziaVetor, x => Console.Write(x + " "));
-                    Console.WriteLine();
-                    verificarEntrada = char.TryParse(Console.ReadLine(), out char caractere);
+                    
+                    Utilidade.ImprimirPalavra(palavraVaziaVetor);
+                    
+                    entrada = char.TryParse(Console.ReadLine(), out char caractere);
+                    resultadoEntrada = Utilidade.VerificarEntrada(entrada, caractere, letrasDigitadas);
 
-                    if (!verificarEntrada || !char.IsLetter(caractere))
+                    if (resultadoEntrada)
                     {
-                        Console.Clear();
-                        Console.WriteLine("Digite uma letra válida\n");
-                        continue;
-                    }
-
-                    if (letrasDigitadas.Contains(caractere))
-                    {
-                        Console.Clear();
-                        Console.WriteLine("Letra já inserida anteriormente\n");
                         continue;
                     }
 
                     letrasDigitadas.Add(caractere);
 
-                    if (palavra.Contains(caractere))
-                    {
-                        for (int i = 0; i < palavra.Length; i++)
-                        {
-                            if (palavra[i] == caractere)
-                            {
-                                palavraVaziaVetor[i] = caractere;
-                            }
-                        }
-                        Console.Clear();
-                    }
-                    else
-                    {
-                        Console.Clear();
-                        Console.WriteLine("A letra digitada não existe na palavra-chave\n");
-                        tentativas--;
-                        
-                    }
+                    Utilidade.VerificarLetraExiste(palavra, ref tentativas, caractere, palavraVaziaVetor);
 
-                    if (tentativas == 0)
-                    {
-                        Console.Clear();
-                        Console.WriteLine("Que pena! Suas tentativas acabaram e você não acertou a palavra!\n");
-                        acertou = true;
-                    }
+                    acabou = Utilidade.VerificarFim(tentativas, palavraVaziaVetor);
 
-                    if (!palavraVaziaVetor.Contains('_'))
-                    {
-                        Console.Clear();
-                        Console.WriteLine($"Parabéns! Você acertou\n");
-                        acertou = true;
-                    }
-
-                } while (!acertou);
+                } while (!acabou);
 
                 Console.WriteLine($"A palavra era -> {palavra}\n");
+               
                 Console.WriteLine("Deseja jogar novamente?\nSim[1]\t\tNão[0]\n");
                 _ = char.TryParse(Console.ReadLine(), out char respostaSaida);
                 if(respostaSaida != '1')
